6a05d1b92fda5b958592c526f92e4e4a4f724479
adam <adam@jubelio.com>
2023-07-30 08:48:24 +0700

    addmin readme and removing logs in unit testing

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # voucher-system
+A system that will generate voucher for user.
+
+Database preparation:
+```javascript
+go to 'migarations' folder
+open 'preparation.sql' file
+copy 
+paste to your DBMS
+run it
+```
+
+How to install and run system:
+```javascript
+cd vocher-system
+npm install
+node index.js
+```
+
+Data preparation:
+```javascript
+run 'npm test'
+```
--- test/test.js
+++ test/test.js
@@ -7,26 +7,27 @@ db.configure({
     database: "simple_crud"
 });
 
-xit ('create vouchers', async () => {
-    for (let index = 0; index < 100; index++) {
-        let r = (Math.random() + 1).toString(36).substring(7);
-        await db.query(`INSERT INTO voucher (voucher_code) values (?)`, [r]);
-        console.log('success');
-    }
-});
-
-xit ('create users', async () => {
+// create random Users
+it ('create users', async () => {
     for (let index = 0; index < 5; index++) {
+        let userType = 'customer';
         let r = (Math.random() + 1).toString(36).substring(7);
-        await db.query(`INSERT INTO user (user_code, type) values (?, 'client')`, [r.toUpperCase()]);
-        console.log('success');
+        await db.query(`INSERT INTO user (user_code, type) values (?, ?)`, [r.toUpperCase(), userType]);
     }
 });
 
+// create random Vouchers
+it ('create vouchers', async () => {
+    for (let index = 0; index < 1000; index++) {
+        let r = (Math.random() + 1).toString(36).substring(7);
+        await db.query(`INSERT IGNORE INTO voucher (voucher_code) values (?)`, [r]);
+    }
+});
+
+// create random Transactions or Purchases
 it ('create transactions or puchases', async () => {
     for (let index = 0; index < 5; index++) {
-        const user_id = 4;
+        const user_id = 1;
         await db.query(`INSERT INTO purchase (user_id, price, status) values (?, ?, ?)`, [user_id, 10000, 'COMPLETED']);
-        console.log('success');
     }
 });
